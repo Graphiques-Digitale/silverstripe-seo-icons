ded85c05a152aec55f01607ed3dddda27fc6411f
Andrew <atari@graphiquesdigitale.net>
2016-06-30 21:09:30 +0100

    Major update bringing into line with silverstripe-seo-metadata

--- code/SEO_Icons_SiteConfig_DataExtension.php
+++ code/SEO_Icons_SiteConfig_DataExtension.php
@@ -1,34 +1,59 @@
 <?php
 
 /**
- * SEO_Icons_SiteConfig_DataExtension
+ * @todo Description
  *
- * @todo add description
+ * I called them Pinicons & Tabicons in keeping with the absolutely pointless name: Favicons.
  *
  * @package silverstripe-seo
  * @subpackage icons
  * @author Andrew Gerber <atari@graphiquesdigitale.net>
  * @version 1.0.0
  *
- * @todo lots
- *
  */
 
 class SEO_Icons_SiteConfig_DataExtension extends DataExtension {
 
 
+	/* Static Variables
+	------------------------------------------------------------------------------*/
+
+	//
+	private static $SEOIconsUpload = 'SEO/Icons/';
+
+	//
+	const APPLE_ICON_DEFAULT_BACKGROUND = '000000';
+
+	
 	/* Overload Model
 	------------------------------------------------------------------------------*/
 
 	private static $db = array(
-		// Favicon background for M$-IE
-		'FaviconBG' => 'Varchar(6)',
+		// Pinned Icon Title
+		'PiniconTitle' => 'Varchar(128)',
+		// IOS Pinned Icon
+//		'IOSPiniconBackgroundColor' => 'Varchar(6)',
+		// Android Pinned Icon
+		'AndroidPiniconThemeColor' => 'Varchar(6)',
+		// MS Tile
+		'WindowsPiniconBackgroundColor' => 'Varchar(6)',
+		// Safari Pinned Tab
+//		'SafariTabiconThemeColor' => 'Varchar(6)', // @todo maybe implement
 	);
 	private static $has_one = array(
 		// Favicon
-		'FaviconPNG' => 'Image',
-		// Touch Icon
-		'TouchIcon' => 'Image',
+//		'HTML4Favicon' => 'Image', // @todo maybe implement
+		'HTML5Favicon' => 'Image',
+		// Apple Pinned Icon
+		'IOSPinicon' => 'Image',
+		// Android Pinned Icon
+		'AndroidPinicon' => 'Image',
+		// Windows Pinned Icon
+		'WindowsPinicon' => 'Image',
+		// Safari Pinned Tab
+//		'SafariTabiconDefault' => 'Image', // @todo maybe implement
+//		'SafariTabiconWide' => 'Image', // @todo maybe implement
+//		'SafariTabiconLarge' => 'Image', // @todo maybe implement
 	);
 
 
@@ -39,52 +64,278 @@ class SEO_Icons_SiteConfig_DataExtension extends DataExtension {
 	public function updateCMSFields(FieldList $fields) {
 
 		// owner
+		$config = SiteConfig::current_site_config();
 		$owner = $this->owner;
 
-		// SSSEO Tabset
-// 		$fields->addFieldToTab('Root', new TabSet('SEO'));
+		//// Favicons Tab
+
+		$tab = 'Root.Metadata.Favicons';
 
 		//// Favicon
 
-		$tab = 'Root.SEO.Favicon';
+		$fields->addFieldsToTab($tab, array(
+			LabelField::create('FaviconDescription', 'Favicons are `favourite icons` used by browsers in a number of ways whenever an icon is necessary e.g. tabs, history, bookmarks, dashboards.<br />@ <a href="https://en.wikipedia.org/wiki/Favicon" target="_blank">Favicon - Wikipedia, the free encyclopedia</a>')
+				->addExtraClass('information')
+		));
 
-		// ICO
+		//// HTML4 Favicon
+
+		// check favicon.ico & set status
+		$icoStatus = ReadonlyField::create('HTML4FaviconStatus', 'Favicon ICO<pre>type: ico</pre><pre>size: (multiple)<br />16x16 & 32x32 & 64x64 px</pre>', 'favicon.ico error');
 		if (Director::fileExists('favicon.ico')) {
-			$fields->addFieldsToTab($tab, array(
-				ReadonlyField::create('ReadonlyFaviconICO', 'Favicon ICO', 'favicon.ico found')
-					->addExtraClass('success')
-			));
+			$icoStatus
+				->setValue('favicon.ico found')
+				->addExtraClass('success favicon');
 		} else {
-			$fields->addFieldsToTab($tab, array(
-				ReadonlyField::create('ReadonlyFaviconICO', 'Favicon ICO', 'favicon.ico not found')
-					->addExtraClass('error')
-			));
+			$icoStatus
+				->setValue('favicon.ico not found')
+				->addExtraClass('error');
 		}
 
-		// PNG
+		// header & fields
 		$fields->addFieldsToTab($tab, array(
-			UploadField::create('FaviconPNG', 'Favicon PNG')
+			HeaderField::create('HTML4FaviconHeader', 'HTML4 <span class="aka">( favicon.ico )</span>'),
+			LabelField::create('HTML4FaviconDescription', 'It is recommended you simply have a `favicon.ico` file in the webroot.')
+				->addExtraClass('information'),
+			$icoStatus
+		));
+
+		//// HTML5 Favicon
+
+		// header & fields
+		$fields->addFieldsToTab($tab, array(
+			HeaderField::create('HTML5FaviconHeader', 'HTML5 <span class="aka">( favicon.png )</span>'),
+			LabelField::create('HTML5FaviconDescription', '@todo Description')
+				->addExtraClass('information'),
+			UploadField::create('HTML5Favicon', 'Favicon PNG<pre>type: png</pre><pre>size: 192x192 px</pre>')
 				->setAllowedExtensions(array('png'))
 				->setFolderName('SiteConfig/seo-icons/')
-				->setDescription('file format: PNG' . '<br />' . 'pixel dimensions: 152 x 152'),
-			TextField::create('FaviconBG', 'IE10 Tile Background')
-				->setAttribute('placeholder', $owner->faviconBGDefault())
-				->setAttribute('size', 6)
-				->setMaxLength(6)
-				->setDescription('format: hexadecimal triplet<br />character limit: 6')
 		));
 
-		//// Touch Icon
+		//// Pinned Icons Tab
 
-		$tab = 'Root.SEO.TouchIcon';
+		$tab = 'Root.Metadata.PinnedIcons';
+
+		//// Pinned Icons Information
 
 		$fields->addFieldsToTab($tab, array(
-			ReadonlyField::create('AppleTouchIconPrecomposed', 'apple-touch-icon-precomposed', 'on'),
-			UploadField::create('TouchIcon', 'Touch Icon')
-				->setAllowedExtensions(array('jpg', 'jpeg', 'png', 'gif'))
-				->setFolderName('SiteConfig/seo-icons/')
-				->setDescription('file format: JPG, PNG, GIF<br />pixel dimensions: 400 x 400 (recommended, minimum 192)<br />pixel ratio: 1:1')
+			LabelField::create('PiniconDescription', 'Pinned icons are OS-specific desktop shortcuts to pages on your website, they allow you to configure additional theming options to make pages appear more `native` / `web-app-y` within the OS.<br />Given they are OS-specific, they (obviously!) have a different format for each one :(')
+				->addExtraClass('information')
 		));
+
+		//// Pinned Icon Title
+
+		// CMS fields
+		$fields->addFieldsToTab($tab, array(
+			// header
+			HeaderField::create('PiniconTitleHeader', 'Pinned Icon Title <span class="aka">( a.k.a. App Name )</span>'),
+			// description
+			LabelField::create('PiniconTitleDescription', 'When adding a link to the home screen, the user can choose a caption. By default, this is the bookmarked page title, which is usually fine. However, iOS and Windows 8 let you override this default value.')
+				->addExtraClass('information'),
+			TextField::create('PiniconTitle', 'Application Title')
+				->setAttribute('placeholder', 'default: page title')
+		));
+
+		//// iOS Pinned Icon
+
+		// CMS fields
+		$fields->addFieldsToTab($tab, array(
+			// header
+			HeaderField::create('IOSPiniconHeader', 'iOS Pinned Icon <span class="aka">( a.k.a. Touch Icons, Web Clips )</span>'),
+			// information
+			LabelField::create('IOSPiniconDescription', 'iPhone and iPad users can pin your web site on their home screen. The link looks like a native app.<br />@ <a href="https://developer.apple.com/library/ios/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html" target="_blank">Configuring Web Applications - iOS Developer Library</a>')
+				->addExtraClass('information'),
+			// icon
+			UploadField::create('IOSPinicon', 'iOS Icon<pre>type: png</pre><pre>size: 192x192 px</pre>')
+				->setAllowedExtensions(array('png'))
+				->setFolderName('SiteConfig/seo-icons/')
+				->setDescription('iOS will fill the transparent regions with black by default, so put your own background in!'),
+			// background
+//			TextField::create('IOSPiniconBackgroundColor', 'Background Color<pre>type: hex triplet</pre>')
+//				->setAttribute('placeholder', self::APPLE_ICON_DEFAULT_BACKGROUND)
+//				->setAttribute('size', 6)
+//				->setMaxLength(6)
+//				->setDescription('iOS will fill the transparent regions with black by default, so put your own background in.')
+		));
+
+		//// Android Pinned Icon
+
+		// CMS fields
+		$fields->addFieldsToTab($tab, array(
+			// header
+			HeaderField::create('AndroidPiniconHeader', 'Android Pinned Icon <span class="aka">( a.k.a. Android Chrome Icons, Launcher Icons )</span>'),
+			// information
+			LabelField::create('AndroidPiniconDescription', 'Add to Homescreen is a also a feature of Android Chrome. Your visitors can mix their natives apps and web bookmarks.<br />@ <a href="https://developer.chrome.com/multidevice/android/installtohomescreen" target="_blank">Add to Homescreen - Google Chrome</a>')
+				->addExtraClass('information'),
+			// icon
+			UploadField::create('AndroidPinicon', 'Android Icon<pre>type: png</pre><pre>size: 192x192 px</pre>')
+				->setAllowedExtensions(array('png'))
+				->setFolderName('SiteConfig/seo-icons/'),
+			// background
+			TextField::create('AndroidPiniconThemeColor', 'Theme Color<pre>type: hex triplet</pre>')
+				->setAttribute('placeholder', 'none')
+				->setAttribute('size', 6)
+				->setMaxLength(6)
+				->setDescription('Starting with Android Lollipop, you can customize the color of the task bar in the switcher.')
+		));
+
+		//// Windows Pinned Icon
+
+		// CMS fields
+		$fields->addFieldsToTab($tab, array(
+			// header
+			HeaderField::create('WindowsShortcutHeader', 'Windows Pinned Icon <span class="aka">( a.k.a. Windows 8 / Metro Tiles )</span>'),
+			// information
+			LabelField::create('WindowsShortcutDescription', 'Windows 8 users can pin your web site on their desktop. Your site appears as a tile, just like a native Windows 8 app.<br />@ <a href="https://msdn.microsoft.com/en-us/library/dn455106" target="_blank">Creating custom tiles for IE11 websites (Windows)</a>')
+				->addExtraClass('information'),
+			// icon
+			UploadField::create('WindowsPinicon', 'Windows Icon<pre>type: png</pre><pre>size: 192x192 px</pre>')
+				->setAllowedExtensions(array('png'))
+				->setFolderName('SiteConfig/seo-icons/'),
+			// background
+			TextField::create('WindowsPiniconBackgroundColor', 'Background ( Tile ) Color<pre>type: hex triplet</pre>')
+				->setAttribute('placeholder', 'none')
+				->setAttribute('size', 6)
+				->setMaxLength(6)
+		));
+
+		// @todo Safari Pinned Tab ~ maybe ??
+
+	}
+
+	public function onAfterWrite() {
+		
+		// parent
+		parent::onAfterWrite();
+		
+		// regenerate manifest
+		if ($this->generateAndroidManifest()) {
+			// @todo SilverStripe success message
+		} else {
+			// @todo SilverStripe failure message
+		}
+		
+	}
+
+
+	/* Custom Methods
+	------------------------------------------------------------------------------*/
+
+	//// fetch functions
+
+	public function fetchPiniconTitle() {
+
+		if ($this->owner->PiniconTitle) {
+			// return pinicon title
+			return $this->owner->PiniconTitle;
+		} else {
+			// default
+			return false;
+		}
+
+	}
+
+	public function fetchAndroidPiniconThemeColor() {
+
+		if ($this->owner->AndroidPiniconThemeColor) {
+			return '#' . $this->owner->AndroidPiniconThemeColor;
+		} else {
+			return false;
+		}
+
+	}
+
+	public function fetchWindowsPiniconBackgroundColor() {
+
+		if ($this->owner->WindowsPiniconBackgroundColor) {
+			return '#' . $this->owner->WindowsPiniconBackgroundColor;
+		} else {
+			return false;
+		}
+
+	}
+
+	//// generate functions
+
+	public function generateAndroidManifest() {
+
+		//// Android Pinicon Manifest
+
+		$pinicon = $this->owner->AndroidPinicon();
+
+		if ($pinicon->exists()) {
+
+			//
+			$manifest = new stdClass();
+
+			//
+			$manifest->name = $this->owner->PiniconTitle;
+//			$manifest->start_url = null; @todo Maybe implement
+//			$manifest->display = null; @todo Maybe implement
+//			$manifest->orientation = null; @todo Maybe implement
+			$manifest->icons = array();
+
+			// 0.75x density icon
+			array_push($manifest->icons, array(
+				'src' => $pinicon->Fill(36,36)->getAbsoluteURL(),
+				'sizes' => '36x36',
+				'type' => 'image/png',
+				'density' => 0.75
+			));
+
+			// 1x density icon
+			array_push($manifest->icons, array(
+				'src' => $pinicon->Fill(48,48)->getAbsoluteURL(),
+				'sizes' => '48x48',
+				'type' => 'image/png',
+				'density' => 1
+			));
+
+			// 1.5x density icon
+			array_push($manifest->icons, array(
+				'src' => $pinicon->Fill(72,72)->getAbsoluteURL(),
+				'sizes' => '72x72',
+				'type' => 'image/png',
+				'density' => 1.5
+			));
+
+			// 2x density icon
+			array_push($manifest->icons, array(
+				'src' => $pinicon->Fill(96,96)->getAbsoluteURL(),
+				'sizes' => '96x96',
+				'type' => 'image/png',
+				'density' => 2
+			));
+
+			// 3x density icon
+			array_push($manifest->icons, array(
+				'src' => $pinicon->Fill(144,144)->getAbsoluteURL(),
+				'sizes' => '144x144',
+				'type' => 'image/png',
+				'density' => 3
+			));
+
+			// 4x density icon
+			array_push($manifest->icons, array(
+				'src' => $pinicon->Fill(192,192)->getAbsoluteURL(),
+				'sizes' => '192x192',
+				'type' => 'image/png',
+				'density' => 4
+			));
+
+			// create file
+			$bytes = file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/manifest.json', json_encode($manifest));
+
+			//
+			if ($bytes !== false) {
+				// success
+				return true;
+			} else {
+				// failure
+				return false;
+			}
+
+		}
 
 	}
 
--- code/SEO_Icons_SiteTree_DataExtension.php
+++ code/SEO_Icons_SiteTree_DataExtension.php
@@ -1,16 +1,12 @@
 <?php
 
 /**
- * SEO_Icons_SiteTree_DataExtension
- *
- * @todo add description
+ * @todo Description
  *
  * @package silverstripe-seo
  * @subpackage icons
  * @author Andrew Gerber <atari@graphiquesdigitale.net>
  * @version 1.0.0
- *
- * @todo lots
  *
  */
 
@@ -35,99 +31,164 @@ class SEO_Icons_SiteTree_DataExtension extends DataExtension {
 
 	/**
 	 * @name updateMetadata
+	 *
+	 * Updates metadata with icons.
+	 *
+	 * @param SiteConfig $config
+	 * @param SiteTree $owner
+	 * @param $metadata
+	 * @return void
 	 */
-	public function updateMetadata(&$metadata, $owner, $config) {
+	public function updateMetadata(SiteConfig $config, SiteTree $owner, &$metadata) {
 
-		// variables
-// 		$config = SiteConfig::current_site_config();
-// 		$owner = $this->owner;
+		//// HTML4 Favicon
 
-		//// Favicon
+		// @todo Perhaps create dynamic image, but just use favicon.ico for now
 
-		$ico = Director::fileExists('favicon.ico');
+		//// Create Favicons
 
-		// PNG + ICO
-		if ($config->FaviconPNG()->exists()) {
+		$HTML5Favicon = $config->HTML5Favicon();
+		$IOSPinicon = $config->IOSPinicon();
+		$AndroidPinicon = $config->AndroidPinicon();
+		$WindowsPinicon = $config->WindowsPinicon();
 
-			//
-			$pngURL = $config->FaviconPNG()->SetSize(152, 152)->getAbsoluteURL();
-			$pngBG = ($config->FaviconBG) ? $config->FaviconBG : $config->faviconBGDefault();
+		//// iOS Pinicon
 
-			//
-			$metadata .= $owner->MarkupHeader('Favicon');
+		if ($IOSPinicon->exists()) {
 
-			// 1. favicon.png
-			$metadata .= $owner->MarkupRel('icon', $pngURL);
-
-			// 2. favicon.ico
-			if ($ico) {
-				// IE all-but-10
-				$metadata .= '<!--[if IE]><link rel="shortcut icon" href="/favicon.ico" /><![endif]-->' . PHP_EOL;
+			// header
+			$metadata .= $owner->MarkupComment('iOS Pinned Icon');
+			
+			//// iOS Pinicon Title
+			if ($config->fetchPiniconTitle()) {
+				$metadata .= $owner->MarkupMeta('apple-mobile-web-app-title', $config->fetchPiniconTitle());
 			}
 
-			// IE 10
-			$metadata .= $owner->Markup('msapplication-TileColor', $pngBG, false);
-			$metadata .= $owner->Markup('msapplication-TileImage', $pngURL, false);
+			//// iOS Pinned Icon
+
+			// For non-Retina (@1× display) iPhone, iPod Touch, and Android 2.1+ devices
+			$metadata .= $owner->MarkupLink('apple-touch-icon', $IOSPinicon->SetSize(57,57)->getAbsoluteURL(), 'image/png'); // 57×57
+
+			// @todo: What is this for ??
+			$metadata .= $owner->MarkupLink('apple-touch-icon', $IOSPinicon->SetSize(60,60)->getAbsoluteURL(), 'image/png', '60x60');
+
+			// For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≤ 6
+			$metadata .= $owner->MarkupLink('apple-touch-icon', $IOSPinicon->SetSize(72,72)->getAbsoluteURL(), 'image/png', '72x72');
+
+			// For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≥ 7
+			$metadata .= $owner->MarkupLink('apple-touch-icon', $IOSPinicon->SetSize(76,76)->getAbsoluteURL(), 'image/png', '76x76');
+
+			// For iPhone with @2× display running iOS ≤ 6
+			$metadata .= $owner->MarkupLink('apple-touch-icon', $IOSPinicon->SetSize(114,114)->getAbsoluteURL(), 'image/png', '114x114');
+
+			// For iPhone with @2× display running iOS ≥ 7
+			$metadata .= $owner->MarkupLink('apple-touch-icon', $IOSPinicon->SetSize(120,120)->getAbsoluteURL(), 'image/png', '120x120');
+
+			// For iPad with @2× display running iOS ≤ 6
+			$metadata .= $owner->MarkupLink('apple-touch-icon', $IOSPinicon->SetSize(144,144)->getAbsoluteURL(), 'image/png', '144x144');
+
+			// For iPad with @2× display running iOS ≥ 7
+			$metadata .= $owner->MarkupLink('apple-touch-icon', $IOSPinicon->SetSize(152,152)->getAbsoluteURL(), 'image/png', '152x152');
+
+			// For iPhone 6 Plus with @3× display
+			$metadata .= $owner->MarkupLink('apple-touch-icon', $IOSPinicon->SetSize(180,180)->getAbsoluteURL(), 'image/png', '180x180');
+
+		} else {
+
+			// @todo: hmm??
+//			// disabled header
+//			$metadata .= $owner->MarkupComment('iOS Pinned Icon DISABLED');
+			
+		}
+
+		//// Android Pinicon
+
+		// @todo: Should `rel="icon" sizes="192x192"` be the HTML5 icon or the Android icon ??
+
+//		if ($AndroidPinicon->exists()) {
+//
+//			// Android Chrome 37+
+//			$metadata .= $owner->MarkupLink('icon', $AndroidPinicon->SetSize(192,192)->getAbsoluteURL(), 'image/png', '192x192');
+//
+//		}
+
+		//// HTML5 Favicon
+
+		if ($HTML5Favicon->exists()) {
+
+			// header
+			$metadata .= $owner->MarkupComment('HTML5 Favicon');
+
+//			// Android Chrome 32
+			// @todo: Is the Android Chrome 32 196x196 px icon fully redundant ??
+//			$metadata .= $owner->MarkupLink('icon', $HTML5Favicon->SetSize(196,196)->getAbsoluteURL(), 'image/png', '196x196');
+
+			// Android Chrome 37+ / HTML5 spec
+			$metadata .= $owner->MarkupLink('icon', $HTML5Favicon->SetSize(192,192)->getAbsoluteURL(), 'image/png', '192x192');
+
+			// Android Chrome 37+ / HTML5 spec
+			$metadata .= $owner->MarkupLink('icon', $HTML5Favicon->SetSize(128,128)->getAbsoluteURL(), 'image/png', '128x128');
+
+			// For Google TV
+			$metadata .= $owner->MarkupLink('icon', $HTML5Favicon->SetSize(96,96)->getAbsoluteURL(), 'image/png', '96x96');
+
+			// For Safari on Mac OS
+			$metadata .= $owner->MarkupLink('icon', $HTML5Favicon->SetSize(32,32)->getAbsoluteURL(), 'image/png', '32x32');
+
+			// The classic favicon, displayed in the tabs
+			$metadata .= $owner->MarkupLink('icon', $HTML5Favicon->SetSize(16,16)->getAbsoluteURL(), 'image/png', '16x16');
 
 		}
 
-		// ICO only
-		else {
-			if ($ico) {
-				$metadata .= $owner->MarkupHeader('Favicon');
-				$metadata .= $owner->MarkupRel('shortcut icon', '/favicon.ico');
+		//// Android Pinicon Manifest
+
+		if ($AndroidPinicon->exists()) {
+
+			// header
+			$metadata .= $owner->MarkupComment('Android Pinned Icon');
+
+			//
+			if ($config->fetchAndroidPiniconThemeColor()) {
+				$metadata .= $owner->MarkupMeta('theme-color', $config->fetchAndroidPiniconThemeColor());
 			}
-		}
 
-		//// Touch Icon
-
-		$image = $config->TouchIcon();
-
-		if ($image->exists()) {
-
-			// variables
-			$metadata .= $owner->MarkupHeader('Touch Icon');
-
-			// 192 x 192
-// 			$metadata .= '<!-- For Chrome for Android: -->';
-			$metadata .= '<link rel="icon" sizes="192x192" href="' . $image->SetSize(192, 192)->getAbsoluteURL() . '">' . PHP_EOL;
-
-			// 180 x 180
-// 			$metadata .= '<!-- For iPhone 6 Plus with @3× display: -->';
-			$metadata .= '<link rel="apple-touch-icon-precomposed" sizes="180x180" href="' . $image->SetSize(180, 180)->getAbsoluteURL() . '">' . PHP_EOL;
-
-			// 152 x 152
-// 			$metadata .= '<!-- For iPad with @2× display running iOS ≥ 7: -->';
-			$metadata .= '<link rel="apple-touch-icon-precomposed" sizes="152x152" href="' . $image->SetSize(152, 152)->getAbsoluteURL() . '">' . PHP_EOL;
-
-			// 144 x 144
-// 			$metadata .= '<!-- For iPad with @2× display running iOS ≤ 6: -->';
-			$metadata .= '<link rel="apple-touch-icon-precomposed" sizes="144x144" href="' . $image->SetSize(144, 144)->getAbsoluteURL() . '">' . PHP_EOL;
-
-			// 120 x 120
-// 			$metadata .= '<!-- For iPhone with @2× display running iOS ≥ 7: -->';
-			$metadata .= '<link rel="apple-touch-icon-precomposed" sizes="120x120" href="' . $image->SetSize(120, 120)->getAbsoluteURL() . '">' . PHP_EOL;
-
-			// 114 x 114
-// 			$metadata .= '<!-- For iPhone with @2× display running iOS ≤ 6: -->';
-			$metadata .= '<link rel="apple-touch-icon-precomposed" sizes="114x114" href="' . $image->SetSize(114, 114)->getAbsoluteURL() . '">' . PHP_EOL;
-
-			// 76 x 76
-// 			$metadata .= '<!-- For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≥ 7: -->';
-			$metadata .= '<link rel="apple-touch-icon-precomposed" sizes="76x76" href="' . $image->SetSize(76, 76)->getAbsoluteURL() . '">' . PHP_EOL;
-
-			// 72 x 72
-// 			$metadata .= '<!-- For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≤ 6: -->';
-			$metadata .= '<link rel="apple-touch-icon-precomposed" sizes="72x72" href="' . $image->SetSize(72, 72)->getAbsoluteURL() . '">' . PHP_EOL;
-
-			// 57 x 57
-// 			$metadata .= '<!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->';
-			$metadata .= '<link rel="apple-touch-icon-precomposed" href="' . $image->SetSize(57, 57)->getAbsoluteURL() . '"><!-- 57×57px -->' . PHP_EOL;
+			//
+			$metadata .= $owner->MarkupLink('manifest', '/manifest.json');
 
 		}
 
-		// return
-		return $metadata;
+		//// Windows Pinicon Manifest
+
+		if ($WindowsPinicon->exists()) {
+
+			// header
+			$metadata .= $owner->MarkupComment('Windows Pinned Icon');
+
+			// application name
+			$appName = $config->fetchPiniconTitle();
+			if (!$appName) $appName = $config->Title;
+			$metadata .= $owner->MarkupMeta('application-name', $appName);
+
+			// tile background color
+			if ($config->fetchWindowsPiniconBackgroundColor()) {
+				$metadata .= $owner->MarkupMeta('msapplication-TileColor', $config->fetchWindowsPiniconBackgroundColor());
+			}
+
+			// small tile
+			$metadata .= $owner->MarkupMeta('msapplication-square70x70logo', $WindowsPinicon->Fill(70,70)->getAbsoluteURL());
+
+			// medium tile
+			$metadata .= $owner->MarkupMeta('msapplication-square150x150logo', $WindowsPinicon->Fill(150,150)->getAbsoluteURL());
+
+			// @todo: Implement wide & tall tiles
+
+			// wide tile
+//			$metadata .= $owner->MarkupMeta('msapplication-square310x150logo', $WindowsPinicon->Fill(310,150)->getAbsoluteURL());
+
+			// large tile
+//			$metadata .= $owner->MarkupMeta('msapplication-square310x310logo', $WindowsPinicon->Fill(310,310)->getAbsoluteURL());
+
+		}
 
 	}
 
